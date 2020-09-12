92ee2a567157ce67ac60d67fcb0f40016ef87ebc
Guillaume C <42325924+aemi-dev@users.noreply.github.com>
2020-09-13 00:42:43 +0200

    Add JSHint & Update ESLint

--- index.js
+++ index.js
@@ -6,42 +6,42 @@ const port = 3000;
 
 
 // EJS
-app.set("views", "./views");
-app.set("view engine", "ejs");
+app.set( 'views', './views' );
+app.set( 'view engine', 'ejs' );
 
 // Static
 app.use( compression( { level: 1 } ) );
 app.use( express.static( 'public' ) );
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.get( '/', ( req, res ) => {
+	res.render( 'home' );
+} );
 
-app.get('/cours-profs', (req, res) => {
-  res.render('cours-profs');
-});
+app.get( '/cours-profs', ( req, res ) => {
+	res.render( 'cours-profs' );
+} );
 
-app.get('/outils', (req, res) => {
-  res.render('outils');
-});
+app.get( '/outils', ( req, res ) => {
+	res.render( 'outils' );
+} );
 
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
+app.get( '/contact', ( req, res ) => {
+	res.render( 'contact' );
+} );
 
 // Search
-app.get('/search/:query?', (req, res) => {
-  const query = req.params.query;
-  res.json({search: query});
-});
+app.get( '/search/:query?', ( req, res ) => {
+	const query = req.params.query;
+	res.json( { search: query } );
+} );
 
 // Error page not found
-app.use((req, res, next) => {
-  res.status(404);
-  res.render('errors/404');
-});
+app.use( ( req, res, next ) => {
+	res.status( 404 );
+	res.render( 'errors/404' );
+} );
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+app.listen( port, () => {
+	console.log( `Server listening at http://localhost:${port}` );
+} );
