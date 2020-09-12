06fe030681c4fde79c270394a69937b175c3ee22
Mat <vecch.matteo@gmail.com>
2020-09-12 03:53:46 +0200

    Fix, add 404 page and favicons

--- index.js
+++ index.js
@@ -6,19 +6,30 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/home.html'));
+  res.sendFile(path.join(__dirname + '/views/home.html'));
 });
 
 app.get('/cours-profs', (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/cours-profs.html'));
+  res.sendFile(path.join(__dirname + '/views/cours-profs.html'));
 });
 
 app.get('/outils', (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/outils.html'));
+  res.sendFile(path.join(__dirname + '/views/outils.html'));
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/contact.html'));
+  res.sendFile(path.join(__dirname + '/views/contact.html'));
+});
+
+// Error page not found
+app.use(function(req, res, next){
+  res.status(404);
+
+  res.format({
+    html: function () {
+      res.sendFile(path.join(__dirname + '/views/404.html'));
+    }
+  });
 });
 
 app.listen(port, () => {
